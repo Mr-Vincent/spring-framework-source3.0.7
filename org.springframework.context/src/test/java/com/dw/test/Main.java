417d7603a6f3932bb729e2d8975b85129893332b
dw-pc <dong.wei.love@163.com>
2017-11-30 09:54:32 +0800

    first commit and trans this proj into maven proj

---
+++ org.springframework.context/src/test/java/com/dw/test/Main.java
@@ -0,0 +1,49 @@
+package com.dw.test;
+/*
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　 ┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　  ┃
+ * 　　┃　　　　　　 ┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┃    神兽保佑,代码无bug
+ * 　　　　┃　　　┃
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * <p>
+ * ━━━━━━感觉萌萌哒━━━━━━
+ */
+
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractRefreshableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.EncodedResource;
+
+import java.io.IOException;
+
+/**
+ * @author dongw [dongwei2016@gmail.com]
+ * @date 2017/11/24
+ * @description
+ */
+public class Main {
+	public static void main(String[] args) throws IOException {
+		// create and configure beans
+		AbstractRefreshableApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"services.xml"});
+		ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
+		System.out.println(beanFactory.getBeanDefinitionCount());
+		Person p = context.getBean(Person.class);
+		System.out.println(p.toString());
+//		System.in.read();
+	}
+}
---
+++ org.springframework.context/src/test/java/com/dw/test/Person.java
@@ -0,0 +1,64 @@
+package com.dw.test;/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　 ┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　  ┃
+ * 　　┃　　　　　　 ┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┃    神兽保佑,代码无bug
+ * 　　　　┃　　　┃
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * <p>
+ * ━━━━━━感觉萌萌哒━━━━━━
+ */
+
+/**
+ * @author dongw [dongwei2016@gmail.com]
+ * @date 2017/11/24 
+ * @description
+ */
+public class Person {
+	private String name;
+	private String id;
+
+	public Person() {
+	}
+
+	public Person(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" +
+				"name='" + name + '\'' +
+				", id='" + id + '\'' +
+				'}';
+	}
+}
