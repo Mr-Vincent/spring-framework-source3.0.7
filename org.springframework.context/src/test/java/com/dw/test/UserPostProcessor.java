c56e718dcae469d1b56141ac5d3ab3e7adb351c4
dw <dong.wei.love@163.com>
2017-12-07 21:11:29 +0800

    new class

---
+++ org.springframework.context/src/test/java/com/dw/test/UserPostProcessor.java
@@ -0,0 +1,25 @@
+package com.dw.test;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * 旧版本没有这个特性 也不知该怎么去用
+ */
+public class UserPostProcessor implements BeanPostProcessor {
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("=========postProcessBeforeInitialization============");
+        if(bean instanceof Person){
+            Person person = (Person)bean;
+            person.setName("新名字");
+            person.setId("2542154115");
+            return person;
+        }
+        return bean;
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("=========postProcessAfterInitialization============");
+        return bean;
+    }
+}
